a7ff46b4a3ae5ff3ffb3c79468f92b02c4b2c0df
Alejandro Contreras <kdta15@outlook.com>
2021-05-26 18:52:37 -0500

    Add functions to delete a book. Add localStorage API. Add details on the layout style.

--- assets/scripts/library-script.js
+++ assets/scripts/library-script.js
@@ -1,27 +1,66 @@
 const cardsContainer = document.getElementsByClassName("cards-container")[0];
 
-myLibrary = [];
+function showForm(){
+  let form = document.getElementById("new-book-form");
+  let btn = document.getElementById("new-book-btn")
+  form.style.display = "block";
+  btn.style.display = "none";
+}
 
-function Book(title, pages, status) {
+let myLibrary = [];
+
+function Book(title, pages, status, id) {
   this.title = title;
   this.pages = pages;
   this.status = status;
+  this.id = id;
 }
 
-function addBookToLibrary(title, pages, status) {
-  let newbook = new Book(title, pages, status);
+function addBookToLibrary() {
+  myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
+
+  const title = document.getElementById("book-title").value;
+  const pages = document.getElementById("book-pages").value;
+  const status = document.getElementById("book-status").value;
+  const id = myLibrary.length;
+
+  let newbook = new Book(title, pages, status, id);
   myLibrary.push(newbook);
+
+  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
 }
 
-function showBookData(arr) {
-  for (let i = 0; i < arr.length; i++) {
+function showBookData() {
+  if (localStorage.getItem("myLibrary")) {
+    myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
+  } else {
+    alert("There is no data.. add a book");
+  }
+  console.log(myLibrary);
+  for (let i = 0; i < myLibrary.length; i++) {
     cardsContainer.appendChild(
-      createCard(arr[i].title, arr[i].pages, arr[i].status)
+      createCard(
+        myLibrary[i].title,
+        myLibrary[i].pages,
+        myLibrary[i].status,
+        myLibrary[i].id
+      )
     );
   }
 }
 
-function createCard(title, pages, status) {
+function deleteData(bookID) {
+  myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
+  for (let i = 0; i < myLibrary.length; i++) {
+    if (bookID === myLibrary[i].id) {
+      myLibrary.splice(i, 1);
+      localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+      location.reload();
+    }
+  }
+}
+
+function createCard(title, pages, status, id) {
   const card = document.createElement("div");
   card.classList.add("card", "d-flex");
 
@@ -37,18 +76,21 @@ function createCard(title, pages, status) {
   bookStatus.innerText = `Was it already read? ${status}`;
   bookStatus.className = "status-card";
 
+  const bookID = id;
+  const bookButton = document.createElement("button");
+  bookButton.innerText = `Delete`;
+  bookButton.onclick = function () {
+    deleteData(bookID);
+    return false;
+  };
+  bookButton.className = "form-btn";
+
   card.append(bookTitle);
   card.append(bookPages);
   card.append(bookStatus);
+  card.append(bookButton);
 
   return card;
 }
 
-addBookToLibrary("Title test 1", "100", "Yes");
-addBookToLibrary("Title test 2", "100", "No");
-addBookToLibrary("Title test 3", "100", "No");
-addBookToLibrary("Title test 4", "100", "Yes");
-addBookToLibrary("Title test 5", "100", "No");
-addBookToLibrary("Title test 6", "100", "No");
-
-showBookData(myLibrary);
+showBookData();
