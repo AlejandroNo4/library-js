5f962ea4292b5e2c79064626f239715264c24db2
Alejandro Contreras <kdta15@outlook.com>
2021-05-27 20:29:45 -0500

    Add validations to the form. Fix esLint linters.

--- assets/scripts/library-script.js
+++ assets/scripts/library-script.js
@@ -1,4 +1,4 @@
-const cardsContainer = document.getElementsByClassName("cards-container")[0];
+const cardsContainer = document.getElementsByClassName('cards-container')[0];
 
 let myLibrary = [];
 
@@ -10,55 +10,70 @@ function Book(title, pages, status, id) {
 }
 
 function addBookToLibrary() {
-  if (localStorage.getItem("myLibrary")) {
-    myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
+  if (localStorage.getItem('myLibrary')) {
+    myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
   } else {
     myLibrary = [];
   }
 
-  const title = document.getElementById("book-title").value;
-  const pages = document.getElementById("book-pages").value;
-  const status = document.getElementById("book-status").value;
+  const errorMsg = [];
+
+  const title = document.getElementById('book-title').value;
+  const pages = document.getElementById('book-pages').value;
+  const status = document.getElementById('book-status').value;
   const id = myLibrary.length + 1;
+
+  if (title === '') {
+    errorMsg.push('Title must to be filled');
+  }
+
+  if (pages.match(/^[0-9]*$/) === null || pages === '') {
+    errorMsg.push('Pages must to be a number');
+  }
+
+  if (errorMsg.length > 0) {
+    alert(errorMsg.join(', '));
+    return;
+  }
 
   const newbook = new Book(title, pages, status, id);
   myLibrary.push(newbook);
 
-  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
 }
 
 function deleteData(bookID) {
-  myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
+  myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
   for (let i = 0; i < myLibrary.length; i += 1) {
     if (bookID === myLibrary[i].id) {
       myLibrary.splice(i, 1);
-      localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+      localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
       window.location.reload();
     }
   }
 }
 
 function createCard(title, pages, status, id) {
-  const card = document.createElement("div");
-  card.classList.add("card", "d-flex");
+  const card = document.createElement('div');
+  card.classList.add('card', 'd-flex');
 
-  const bookTitle = document.createElement("h3");
+  const bookTitle = document.createElement('h3');
   bookTitle.innerText = title;
-  bookTitle.className = "book-title-card";
+  bookTitle.className = 'book-title-card';
 
-  const bookPages = document.createElement("p");
+  const bookPages = document.createElement('p');
   bookPages.innerText = `Number of pages: ${pages}`;
-  bookPages.className = "pages-card";
+  bookPages.className = 'pages-card';
 
-  const bookStatus = document.createElement("p");
+  const bookStatus = document.createElement('p');
   bookStatus.innerText = `Was it already read? ${status}`;
-  bookStatus.className = "status-card";
+  bookStatus.className = 'status-card';
 
   const bookID = id;
-  const bookButton = document.createElement("button");
-  bookButton.innerText = "Delete";
+  const bookButton = document.createElement('button');
+  bookButton.innerText = 'Delete';
   bookButton.onclick = () => deleteData(bookID);
-  bookButton.className = "form-btn";
+  bookButton.className = 'form-btn';
 
   card.append(bookTitle);
   card.append(bookPages);
@@ -69,15 +84,15 @@ function createCard(title, pages, status, id) {
 }
 
 function showForm() {
-  const form = document.getElementById("new-book-form");
-  const btn = document.getElementById("new-book-btn");
-  form.style.display = "block";
-  btn.style.display = "none";
+  const form = document.getElementById('new-book-form');
+  const btn = document.getElementById('new-book-btn');
+  form.style.display = 'block';
+  btn.style.display = 'none';
 }
 
 function showBookData() {
-  if (localStorage.getItem("myLibrary")) {
-    myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
+  if (localStorage.getItem('myLibrary')) {
+    myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
   } else {
     return;
   }
@@ -88,17 +103,16 @@ function showBookData() {
         myLibrary[i].title,
         myLibrary[i].pages,
         myLibrary[i].status,
-        myLibrary[i].id
-      )
+        myLibrary[i].id,
+      ),
     );
   }
 }
 
-let buttonClicked = document.getElementById("submit-button");
+const submitClicked = document.getElementById('submit-button');
+submitClicked.addEventListener('click', addBookToLibrary);
 
-buttonClicked.addEventListener("click", () => {
-  addBookToLibrary();
+const showClicked = document.getElementById('new-book-btn');
+showClicked.addEventListener('click', showForm);
 
-});
 showBookData();
-// buttonClicked.addEventListener("click", addBookToLibrary);
